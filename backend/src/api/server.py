275181444131908ca6f23567e9476192016f95f2
Arctic1ce <davariartin@gmail.com>
2023-10-30 09:12:14 -0700

    added book to the server router

--- backend/src/api/server.py
+++ backend/src/api/server.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, exceptions
 from fastapi.responses import JSONResponse
 from pydantic import ValidationError
-from src.api import catalog
+from src.api import catalog, book
 import json
 import logging
 import sys
@@ -33,6 +33,7 @@ app.add_middleware(
 )
 
 app.include_router(catalog.router)
+app.include_router(book.router)
 
 @app.exception_handler(exceptions.RequestValidationError)
 @app.exception_handler(ValidationError)
