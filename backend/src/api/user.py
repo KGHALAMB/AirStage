cdf686c33f24879317e79f5899f5e1ec1a8e91a7
yashwantsathish <46503623+yashwantsathish@users.noreply.github.com>
2023-11-25 13:03:24 -0800

    made user_type enum

--- backend/src/api/user.py
+++ backend/src/api/user.py
@@ -4,6 +4,7 @@ from sqlite3 import Timestamp
 from time import time
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
+from enum import Enum
 #from src.api import auth
 
 import sqlalchemy
@@ -15,10 +16,14 @@ router = APIRouter(
     #dependencies=[Depends(auth.get_api_key)],
 )
 
+class UserType(str, Enum):
+    performer = "performer"
+    venue = "venue"
+
 class User(BaseModel):
     username: str
     password: str
-    user_type: str
+    user_type: UserType
 
 @router.post("/signup/")
 def signup(user: User):
@@ -30,21 +35,21 @@ def signup(user: User):
             user_exists = True
         
         if not user_exists:
-            if user.user_type == "performer" or user.user_type == "venue":
-                if user.user_type == "performer":
-                    type = 0
-                else:
-                    type = 1
-                result = connection.execute(sqlalchemy.text("INSERT INTO users (user_type, username, password) VALUES (:a, :b, :c) RETURNING user_id"),
-                                                {"a": type, "b": user.username, "c": user.password})
-                user_id = result.first()[0]
-                if type == 0:
-                    result = connection.execute(sqlalchemy.text("INSERT INTO performers (name, capacity_preference, price, user_id) VALUES (:a, :b, :c, :d)"),
-                                                    {"a": user.username, "b": 10000, "c": 10000, "d": user_id})
-                else:
-                    result = connection.execute(sqlalchemy.text("INSERT INTO venues (name, location, capacity, price, user_id) VALUES (:a, :b, :c, :d, :d)"),
-                                                    {"a": user.username, "b": "San Francisco", "c": 10000, "d": 10000, "e": user_id})
-                return { "success": True }
+            if user.user_type == UserType.performer:
+                type = 0
+            elif user.user_type == UserType.venue:
+                type = 0
+    
+            result = connection.execute(sqlalchemy.text("INSERT  INTO users (user_type, username, password) VALUES (:a, :b, :c) RETURNING user_id"),
+                                            {"a": type, "b": user.username, "c": user.password})
+            user_id = result.first()[0]
+            if type == UserType.performer.value:
+                result = connection.execute(sqlalchemy.text("INSERT INTO performers (name, capacity_preference, price, user_id) VALUES (:a, :b, :c, :d)"),
+                                                {"a": user.username, "b": 10000, "c": 10000, "d": user_id})
+            else:
+                result = connection.execute(sqlalchemy.text("INSERT INTO venues (name, location, capacity, price, user_id) VALUES (:a, :b, :c, :d, :e)"),
+                                                {"a": user.username, "b": "San Francisco", "c": 10000, "d": 10000, "e": user_id})
+            return { "success": True }
 
     return { "success": False }
 
